10f81cace7a62a94af6c8a981626dcaad382b377
Lucas <lucas.bravi.2008@gmail.com>
2021-09-26 19:47:58 -0300

    Java Egg JDBC

--- src/main/java/com/bravi/Ejercicio1/persistence/ProductoDao.java
+++ src/main/java/com/bravi/Ejercicio1/persistence/ProductoDao.java
@@ -6,50 +6,6 @@ import com.bravi.Ejercicio1.dominio.Producto.Producto;
 
 public final class ProductoDao extends DAO {
 
-    public void guardarProducto(Producto producto) throws Exception {
-        try {
-            if (producto == null) {
-                throw new Exception("Producto vacio");
-            }
-            String sql = "INSERT INTO producto(nombre, precio, codigo_fabricante) VALUES ('" + producto.getNombre()
-                    + "', " + producto.getPrecio() + ", " + producto.getCodigoFabricante() + ")";
-            insertarModificarEliminar(sql);
-        } catch (Exception e) {
-            // TODO: handle exception
-            throw e;
-        } finally {
-            desconectarBase();
-        }
-    }
-
-    public void modificarProducto(Producto producto) throws Exception {
-        try {
-            if (producto == null) {
-                throw new Exception("Producto vacio");
-            }
-            String sql = "UPDATE producto SET nombre = '" + producto.getNombre() + "', precio = " + producto.getPrecio()
-                    + ", codigo_fabricante = " + producto.getCodigoFabricante() + " WHERE codigo = "
-                    + producto.getCodigo() + ";";
-            insertarModificarEliminar(sql);
-        } catch (Exception e) {
-            // TODO: handle exception
-            throw e;
-        }
-    }
-
-    public void eliminarProducto(Producto producto) throws Exception {
-        try {
-            if (producto == null) {
-                throw new Exception("Producto vacio");
-            }
-            String sql = "DELETE FROM producto WHERE codigo = " + producto.getCodigo() + ";";
-            insertarModificarEliminar(sql);
-        } catch (Exception e) {
-            // TODO: handle exception
-            throw e;
-        }
-    }
-
     public void seleccionarTodosProductos() throws Exception {
         try {
             String sql = "SELECT nombre FROM producto";
@@ -120,6 +76,9 @@ public final class ProductoDao extends DAO {
 
     public void insertarProducto(Producto producto) throws Exception {
         try {
+            if (producto == null) {
+                throw new Exception("Producto is null");
+            }
             String sql = "INSERT INTO producto (nombre, precio, codigo_fabricante) VALUES ('" + producto.getNombre()
                     + "', " + producto.getPrecio() + ", " + producto.getCodigoFabricante() + ")";
             insertarModificarEliminar(sql);
@@ -131,12 +90,16 @@ public final class ProductoDao extends DAO {
 
     public void modificarProducto(Producto producto, String nombreProducto) throws Exception {
         try {
+            if (producto == null) {
+                throw new Exception("Producto is null");
+            }
             String sql = "UPDATE producto SET nombre = '" + producto.getNombre() + "', precio = " + producto.getPrecio()
                     + ", codigo_fabricante = " + producto.getCodigoFabricante() + " WHERE nombre = '" + nombreProducto
                     + "';";
             insertarModificarEliminar(sql);
         } catch (Exception e) {
             // TODO: handle exception
+            throw e;
         }
     }
 
